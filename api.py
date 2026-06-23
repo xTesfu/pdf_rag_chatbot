838b274dede019e6d34bc8c55d7942dbb483ed7b
TesfuTe <taffletekleab@gmail.com>
2026-06-23 13:57:59 +0200

    Add APIs to the rag pdf chat.

---
+++ api.py
@@ -0,0 +1,111 @@
+import os
+import tempfile
+from typing import Annotated
+
+from fastapi import (
+    FastAPI,
+    File,
+    UploadFile,
+)
+from pydantic import BaseModel
+
+from app.chunker import chunk_text
+from app.embeddings import build_vector
+from app.llm import ask_llm
+from app.pdf_loader import load_pdf
+from app.retriever import retrieve
+from app.vector_store import (
+    build_index,
+    get_all_documents,
+    get_doc_id,
+    load_chunks,
+    load_index,
+    save_chunks,
+    save_index,
+)
+
+app = FastAPI(
+    title="PDF RAG API",
+    version="1.0.0"
+)
+
+class Question(BaseModel):
+    question: str
+
+@app.post("/upload")
+async def upload_pdfs(
+    files: Annotated[list[UploadFile], File()]
+):
+    uploaded_documents = []
+
+    for file in files:
+
+        pdf_bytes = await file.read()
+        doc_id = get_doc_id(pdf_bytes)
+
+        index = load_index(doc_id)
+        chunks = load_chunks(doc_id)
+
+        if index is None or chunks is None:
+
+            with tempfile.NamedTemporaryFile(
+                delete=False,
+                suffix=".pdf"
+            ) as tmp:
+
+                tmp.write(pdf_bytes)
+                pdf_path = tmp.name
+
+            try:
+                text = load_pdf(pdf_path)
+                chunks = chunk_text(text)
+
+                vectors = build_vector(chunks)
+                index = build_index(vectors)
+
+                save_index(index, doc_id)
+                save_chunks(chunks, doc_id)
+
+            finally:
+                os.remove(pdf_path)
+
+        uploaded_documents.append(
+            {
+                "filename": file.filename,
+                "document_id": doc_id,
+            }
+        )
+
+    return {
+        "message": "PDF(s) uploaded successfully",
+        "documents": uploaded_documents,
+        "count": len(uploaded_documents),
+    }
+
+@app.post("/ask")
+def ask(question: Question):
+
+    context = retrieve(question.question)
+
+    context_text = "\n\n".join(context)
+
+    answer = ask_llm(
+        context_text,
+        question.question
+    )
+
+    return {
+        "question": question.question,
+        "answer": answer,
+        "sources_found": len(context)
+    }
+
+@app.get("/documents")
+def documents():
+
+    docs = get_all_documents()
+
+    return {
+        "documents": docs,
+        "count": len(docs)
+    }
